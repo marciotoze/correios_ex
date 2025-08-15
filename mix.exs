801e6f9c5a1cc88f37ab3d289265e8053d81cffe
Marcio Toze <marcio.toze@gmail.com>
2025-08-14 21:12:15 -0300

    add credo and ex_docs

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule CorreiosEx.MixProject do
   def project do
     [
       app: :correios_ex,
-      version: "0.1.0",
+      version: File.read!("VERSION"),
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "CorreiosEx",
+      source_url: "https://github.com/marciotoze/correios_ex",
+      homepage_url: "https://github.com/marciotoze/correios_ex",
+      docs: &docs/0
     ]
   end
 
@@ -22,8 +26,15 @@ defmodule CorreiosEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "CorreiosEx",
+      extras: ["README.md"]
     ]
   end
 end
