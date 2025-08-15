6b0dda5e74eedc1e33ff98ae03486a57437515d3
Marcio Toze <marcio.toze@gmail.com>
2025-08-14 21:01:55 -0300

    initital commit

---
+++ lib/correios_ex.ex
@@ -0,0 +1,18 @@
+defmodule CorreiosEx do
+  @moduledoc """
+  Documentation for `CorreiosEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CorreiosEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/correios_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule CorreiosExTest do
+  use ExUnit.Case
+  doctest CorreiosEx
+
+  test "greets the world" do
+    assert CorreiosEx.hello() == :world
+  end
+end
